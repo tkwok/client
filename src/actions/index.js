f4c3da7b1be463049fb9182408976e59a5adf344
tkwok <holypaladin07@gmail.com>
2018-08-23 22:24:27 -0700

    First commit

---
+++ src/actions/index.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { AUTH_USER, AUTH_ERROR } from './types';
+
+// action creator
+// return a function instead of an object, a different value type
+// can return action object OR function
+export const signup = (formProps, callback) => async dispatch => {
+        // as function, dispatch as many actions as we 
+        // want with single action creator, control dispatch process
+        // power of redux thunk, alternative to async action creators
+        // difference than redux promise, one action with promise, limited 
+        try {
+                const response = await axios.post('https://auth-base.herokuapp.com/signup', formProps);
+                dispatch({ type: AUTH_USER, payload: response.data.token });
+                localStorage.setItem('token', response.data.token);
+                callback();
+        } catch(err) {
+                // run if anything goes wrong
+                dispatch({ type: AUTH_ERROR, payload: 'Email in use' });
+        }
+};
+
+export const signout = () => {
+        // normal synchronous action creator, no need async dispatch
+        localStorage.removeItem('token');
+
+        return {
+                type: AUTH_USER,
+                payload: ''
+        };
+};
+
+export const signin = (formProps, callback) => async dispatch => {
+        try {
+                const response = await axios.post('https://auth-base.herokuapp.com/signin', formProps);
+                dispatch({ type: AUTH_USER, payload: response.data.token });
+                localStorage.setItem('token', response.data.token);
+                callback();
+        } catch(err) {
+                // run if anything goes wrong
+                dispatch({ type: AUTH_ERROR, payload: 'Invalid login' });
+        }
+};
