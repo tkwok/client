e59d9ab73de10f2eb85434bfda38b84291a4da1a
tkwok <holypaladin07@gmail.com>
2018-12-02 10:47:47 -0800

    polished responsive nav

--- src/components/common/Hero.js
+++ src/components/common/Hero.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 export default function Hero(props) {
     return (
-        <Row className="header-image center color-white">
-            <Col>
-                <h1><b>Welcome to Client</b></h1>
-                <h2>We Welcome You</h2>
-            </Col>
-        </Row>
+        <Container fluid className="hero">
+            <Row className="header-image center color-white">
+                <Col>
+                    <h1><b>Welcome to Client</b></h1>
+                    <h2>We Welcome You</h2>
+                </Col>
+            </Row>
+        </Container>
     )
 }
